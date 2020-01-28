ae219f17d35fa2391689924ad9ff99ae53fd1ff7
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-01-28 18:19:27 -0500

    Update setupmn.sh

--- setupmn.sh
+++ setupmn.sh
@@ -8,11 +8,11 @@ unzip Ebits0172Linux.zip
 
 echo "installed"
 
-./Linux/ebitsd -daemon
+./Ebits0172Linux/ebitsd -daemon
 sleep 5
 MNKEY=$(./Linux/ebits-cli masternode genkey)
 sleep 1
-./Linux/ebits-cli stop
+./Ebits0172Linux/ebits-cli stop
 sleep 5
 echo "generated mnkey"
 
@@ -28,17 +28,9 @@ rpcport=99901
 staking=0
 masternode=1
 masternodeprivkey=$MNKEY
-addnode=155.138.242.159:15350
-addnode=209.250.253.96:15350
-addnode=144.202.48.69:15350
-addnode=134.209.198.90:15350
-addnode=2.111.74.86:15350
-addnode=178.128.34.150:15350
-addnode=138.197.141.68:15350
-addnode=157.230.124.196:15350
 
 EOF
 
-./Linux/ebitsd -daemon -masternode
+./Ebits0172Linux/ebitsd -daemon -masternode
 echo $NODEIP
 echo $MNKEY
