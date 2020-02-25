a16ddc2c268c3e308fcbd5ba32d69fe2702d20d1
Liquid369 <Liquid369@gmail.com>
2020-02-25 05:52:26 -0600

    [Miner] Adjust staking block chosen

--- src/pow.cpp
+++ src/pow.cpp
@@ -126,7 +126,7 @@ unsigned int PoSWorkRequired(const CBlockIndex* pindexLast, const Consensus::Par
 
     int64_t nActualSpacing = 0;
     if (LastPoSBlock->nHeight != 0)
-        nActualSpacing = LastPoSBlock->GetBlockTime() - LastPoSBlock->pprev->GetBlockTime();
+        nActualSpacing = pindexLast->GetBlockTime() - LastPoSBlock->pprev->GetBlockTime();
 
     if (nActualSpacing < 0)
         nActualSpacing = 1;
